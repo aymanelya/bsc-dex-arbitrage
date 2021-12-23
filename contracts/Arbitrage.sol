8626a73783173ea943fa67c2c1be64c4db028715
PhatJay76 <84767554+PhatJay76@users.noreply.github.com>
2021-12-22 21:32:16 -0500

    import it... dummy

--- contracts/Arbitrage.sol
+++ contracts/Arbitrage.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.0;
 
 import './interfaces/WBNB.sol';
+import './interfaces/IChiToken.sol';
+
 import './libraries/PancakeLibrary.sol';
 
 import './interfaces/IUniswapV2Factory.sol';
